05cea99195f257a71218d8439b1e939854bc1045
🐶Bolt <bolt@wangchunyandeMacBook-Pro-4.local>
2018-09-28 20:50:36 +0800

    update

--- test/forEachAsync.test.ts
+++ test/forEachAsync.test.ts
@@ -22,3 +22,27 @@ test('do fetch 5 times', () => {
         })
     });
 });
+
+test('do fetch 5 times but get error', () => {
+    const arr = Array(5).fill(0).map((v, i) => i).sort(() => ~~(Math.random() * 3) - 1);
+
+    return new Promise((resolve, reject) => {
+        forEachAsync(arr, (element, index, next) => {
+            if (element === 3) {
+                next(3);
+            }
+            aFetch(element).then((res: any) => {
+                expect(res.arg0).toBe(arr[index]);
+                next();
+            });
+        }, (err) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve();
+            }
+        })
+    }).catch((err) => {
+        expect(err).toBe(3);
+    });
+});
